2093fcfc880995a82c90ff352e5fbc95f3a7a10b
Shubham-lk-codes <Shubhamlonkar137@gmail.com>
2025-12-02 12:18:10 +0530

    modified:   index.js

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ app.use(cors());
 app.use(morgan('combined'));
 app.use(express.json());
 
+
 // Import routes
 const geoRoutes = require("./routes/geoRoutes");
 const userRoutes = require("./routes/userRoutes");
